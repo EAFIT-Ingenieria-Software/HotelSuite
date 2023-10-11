b540e063adb30dcd583625aa1a925d0731139d87
Miguel Sosa <miguelsosa2003@gmail.com>
2023-10-11 10:32:46 -0500

    Edited some settings

--- HotelSuite/HotelSuite/settings.py
+++ HotelSuite/HotelSuite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4^w10^t-iw&#zu$0&pxv8@_xyy24l30uf5a!l)ez3d5f+&=qqe'
+SECRET_KEY = os.environ.get['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
