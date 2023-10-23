c3ac627c582306027a57eb27ca5a61c427e7961b
Miguel Sosa <miguelsosa2003@gmail.com>
2023-10-23 14:23:08 -0500

    Imp getters and setters for booking

--- HotelSuite/BookingManagement/models.py
+++ HotelSuite/BookingManagement/models.py
@@ -10,3 +10,30 @@ class Booking(models.Model):
     price = models.IntegerField()
     creationDate = models.DateTimeField(auto_now_add=True)
     updateDate = models.DateTimeField(auto_now=True)
+
+    def getId(self) -> int:
+        return self.id
+
+    def getEntrance(self) -> str:
+        return self.entrance
+
+    def setEntrance(self, entrance: str):
+        self.entrance = entrance
+
+    def getDeparture(self) -> str:
+        return self.departure
+
+    def setDeparture(self, departure: str):
+        self.departure = departure
+
+    def getPrice(self) -> int:
+        return self.price
+
+    def setPrice(self, price: int):
+        self.price = price
+
+    def getCreationDate(self) -> str:
+        return self.creationDate
+
+    def getUpdateDate(self) -> str:
+        return self.updateDate
