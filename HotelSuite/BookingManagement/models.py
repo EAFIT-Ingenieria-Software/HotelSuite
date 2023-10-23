7942d578f5e96c08352893ee04d80ee0e01d2bc6
Miguel Sosa <miguelsosa2003@gmail.com>
2023-10-23 14:13:02 -0500

    Add Booking model

--- HotelSuite/BookingManagement/models.py
+++ HotelSuite/BookingManagement/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Booking(models.Model):
+    id = models.AutoField(primary_key=True)
+    entrance = models.TextField()
+    departure = models.TextField()
+    price = models.IntegerField()
+    creationDate = models.DateTimeField(auto_now_add=True)
+    updateDate = models.DateTimeField(auto_now=True)
