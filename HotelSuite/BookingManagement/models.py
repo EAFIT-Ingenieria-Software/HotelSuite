9850eeaee4c57a11fb270432b8d174bf3d0a2eeb
Miguel Sosa <miguelsosa2003@gmail.com>
2023-10-23 14:30:10 -0500

    Add Room model

--- HotelSuite/BookingManagement/models.py
+++ HotelSuite/BookingManagement/models.py
@@ -37,3 +37,46 @@ class Booking(models.Model):
 
     def getUpdateDate(self) -> str:
         return self.updateDate
+
+
+class Room(models.Model):
+    id = models.AutoField(primary_key=True)
+    number = models.IntegerField()
+    type = models.TextField()
+    availability = models.BooleanField()
+    price = models.IntegerField()
+    creationDate = models.DateTimeField(auto_now_add=True)
+    updateDate = models.DateTimeField(auto_now=True)
+
+    def getId(self) -> int:
+        return self.id
+
+    def getNumber(self) -> int:
+        return self.number
+
+    def setNumber(self, number: int):
+        self.number = number
+
+    def getType(self) -> str:
+        return self.type
+
+    def setType(self, type: str):
+        self.type = type
+
+    def getAvailability(self) -> bool:
+        return self.availability
+
+    def setAvailability(self, availability: bool):
+        self.availability = availability
+
+    def getPrice(self) -> int:
+        return self.price
+
+    def setPrice(self, price: int):
+        self.price = price
+
+    def getCreationDate(self) -> str:
+        return self.creationDate
+
+    def getUpdateDate(self) -> str:
+        return self.updateDate
