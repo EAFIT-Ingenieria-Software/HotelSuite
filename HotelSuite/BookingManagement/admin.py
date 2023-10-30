c75fd6a5eae63335e3138eeae7755532989bf181
Miguel Sosa <miguelsosa2003@gmail.com>
2023-10-29 20:25:51 -0500

    Implementar modelos que se administran desde admin

--- HotelSuite/BookingManagement/admin.py
+++ HotelSuite/BookingManagement/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Booking, Room, Item
 
 # Register your models here.
+admin.site.register(Booking)
+admin.site.register(Room)
+admin.site.register(Item)
