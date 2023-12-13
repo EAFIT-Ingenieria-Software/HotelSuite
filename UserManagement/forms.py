3c22ffd42ca980665226cfc9ed443f4dfb70c641
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2023-12-13 15:22:08 -0500

    imp: User registration

---
+++ UserManagement/forms.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .models import User
+
+
+class UserCreationForm(UserCreationForm):
+    second_name = forms.CharField(max_length=30, required=False)
+
+    class Meta:
+        model = User
+        fields = (
+            "first_name",
+            "second_name",
+            "last_name",
+            "email",
+            "phone",
+            "password1",
+        )
+
+    def __init__(self, *args, **kwargs):
+        super(UserCreationForm, self).__init__(*args, **kwargs)
+
+        for fieldname in [
+            "first_name",
+            "second_name",
+            "last_name",
+            "email",
+            "phone",
+            "password1",
+            "password2",
+        ]:
+            self.fields[fieldname].help_text = None
+            self.fields[fieldname].widget.attrs.update({"class": "form-control"})
--- UserManagement/views.py
+++ UserManagement/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserCreationForm
+from django.contrib.auth import login
+from django.db import IntegrityError
+from .models import User
 
 # Create your views here.
+
+
+def SignUp(request):
+    if request.method == "GET":
+        return render(request, "signup.html", {"form": UserCreationForm})
+    elif request.method == "POST":
+        if request.POST.get("password1") == request.POST.get("password2"):
+            try:
+                user = User.objects.create_user(
+                    username=request.POST.get("email"),
+                    first_name=request.POST.get("first_name"),
+                    last_name=request.POST.get("last_name"),
+                    email=request.POST.get("email"),
+                    password=request.POST.get("password1"),
+                )
+                user.set_second_name(request.POST.get("second_name"))
+                user.set_phone(request.POST.get("phone"))
+                user.save()
+                login(request, user)
+                return redirect("home")
+            except IntegrityError:
+                return render(
+                    request,
+                    "signup.html",
+                    {"form": UserCreationForm, "error": "User already exists"},
+                )
+        else:
+            return render(
+                request,
+                "signup.html",
+                {"form": UserCreationForm, "error": "Passwords did not match"},
+            )
