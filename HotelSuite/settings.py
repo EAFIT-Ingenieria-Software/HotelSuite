cc151614e486efe366ae22271ab896fef5bf727f
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-01-04 16:45:01 -0500

    imp: Admin template

--- HotelSuite/settings.py
+++ HotelSuite/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'HotelSuite/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
