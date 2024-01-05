688dbb5843f3fe0e86922b873c054e1b433d0069
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-01-05 15:48:27 -0500

    imp: Admin room index

--- Admin/urls.py
+++ Admin/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, RoomManager
 
 urlpatterns = [
     path('', index, name='index'),
+
+    path('rooms/', RoomManager.index, name='room_manager_index'),
 ]
--- Admin/views.py
+++ Admin/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from BookingManagement.models import Room
 
 # Create your views here.
 
@@ -9,3 +10,14 @@ def index(request):
     template_data['section_title'] = 'Home'
 
     return render(request, 'index.html', {"template_data": template_data})
+
+
+class RoomManager:
+    def index(request):
+        template_data = {}
+        template_data['title'] = 'HotelSuite'
+        template_data['section_title'] = 'Rooms'
+
+        template_data['rooms'] = Room.objects.all()
+
+        return render(request, 'rooms/index.html', {"template_data": template_data})
