d6a53470caf1a097dc8f71d17db9d52cf8bc33bc
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-01-05 14:41:59 -0500

    imp: Admin home page

--- Admin/views.py
+++ Admin/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    template_data = {}
+    template_data['title'] = 'HotelSuite'
+    template_data['section_title'] = 'Home'
+
+    return render(request, 'index.html', {"template_data": template_data})
--- HotelSuite/urls.py
+++ HotelSuite/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
 from Home import views as home
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin/", include("Admin.urls")),
     path("", home.index, name="home"),
     path("user/", include("UserManagement.urls")),
 ]
